3a7cdef204146454c8b0fc52ab0044cddaa53a26
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2021-04-23 14:06:41 -0300

    Cria método para localizar e exibir uma foto a escolha do usuário e adiciona recursos de strings

--- app/src/main/java/com/estudo/bootcampphotos/MainActivity.kt
+++ app/src/main/java/com/estudo/bootcampphotos/MainActivity.kt
@@ -1,11 +1,66 @@
 package com.estudo.bootcampphotos
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonSelectPhoto = findViewById<Button>(R.id.button_select_photo)
+        buttonSelectPhoto.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                    val permission = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    requestPermissions(permission, PERMISSION_CODE)
+                }
+            }
+        }
+    }
+
+    private fun pickImageForGalery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val image_user = findViewById<ImageView>(R.id.image_user)
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            image_user.setImageURI(data?.data)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pickImageForGalery()
+                } else {
+                    Toast.makeText(this, getString(R.string.toast_message), Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        }
+    }
+
+    companion object {
+        private val PERMISSION_CODE = 1000
+        private val IMAGE_PICK_CODE = 1001
     }
 }
